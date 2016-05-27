a78b85a2d07e083acdd2ea9474955ea41c828c48
Samantha Davis <s1dav1s@stanford.edu>
2016-05-26 17:57:30 -0700

    submitted

--- calc.py
+++ calc.py
@@ -15,18 +15,38 @@ def calc(s):
     """Parse a string describing an operation on quantities with units."""
 
     # TODO make this robust for differently formatted inputs
-    num1 = s[0]
-    num2 = s[2]
-    operation = s[1]
+    operators = findOperators(s)
+    numbers = findNumbers(s)
+    num1 = numbers[0]
+    num2 = numbers[1]
+    operation = operators[0]
 
     if operation=='+':
         return int(num1)+int(num2)
     elif operation=='-':
-        pass
+        return int(num1)-int(num2)
     elif operation=='*':
-        pass
+        return int(num1)*int(num2)
     elif operation=='/':
-        pass
+        return int(num1)/int(num2)
 
+def findNumbers(s):
+    numbers = []
+    for j in findOperators(s):
+        s.split(j)
+    for k in s:
+        if (type(k)==float):
+            numbers.append(k)
+    return numbers
+
+def findOperators(s):
+    operators = []
+    for i in s:
+        if (type(i)!=float):
+            operators.append(i)
+    return operators
+    
+        
+        
 
 if __name__ == "__main__": main()
--- testing.py
+++ testing.py
@@ -10,16 +10,16 @@ import calc
 class CalcTest(unittest.TestCase):
     # TODO implement tests here to verify that your functions work!
     def testAddition(self):
-        self.assertEqual(calc.calc('1+1'), 2)
+        self.assertEqual(calc.calc('10+1'), 11)
 
     def testSubtraction(self):
-        pass
+        self.assertEqual(calc.calc('1-1'), 0)
 
     def testMultiplciation(self):
-        pass
+        self.assertEqual(calc.calc('1*2'), 2)
  
     def testDivision(self):
-        pass
+        self.assertEqual(calc.calc('10/5'), 2)
 
 if __name__ == '__main__':
     unittest.main()
